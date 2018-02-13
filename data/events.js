3a1eeed48bdb64e8b4e595bccd97f8e903e92070
Vaidas Pilkauskas <vaidas.pilkauskas@gmail.com>
2018-02-13 17:28:38 +0200

    Add #64

--- data/events.js
+++ data/events.js
@@ -1,5 +1,15 @@
 var events = _.sortByOrder([
 {
+  "id": "64",
+  "type": "Vilnius JUG",
+  "date": "2018-02-21",
+  "time": "19:00",
+  "registration": {
+    "title": "Eventbrite",
+    "url": "http://vilniusjug64.eventbrite.com"
+   }
+},
+{
   "id": "63",
   "type": "Vilnius JUG",
   "date": "2017-11-16",
